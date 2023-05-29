1f5d02db1a15fad5806a82d86736dbe05a8bdca4
jimmy071919 <jimmy071919@gmail.com>
2023-05-29 19:44:55 +0800

    最終

--- time_exp_at_lastrun.py
+++ time_exp_at_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on 五月 28, 2023, at 20:51
+    on 五月 29, 2023, at 19:43
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -670,7 +670,7 @@ for thisPra_trial in pra_trials:
     frameN = -1
     
     # --- Run Routine "rest" ---
-    while continueRoutine and routineTimer.getTime() < 2.0:
+    while continueRoutine and routineTimer.getTime() < 20.0:
         # get current time
         t = routineTimer.getTime()
         tThisFlip = win.getFutureFlipTime(clock=routineTimer)
@@ -690,7 +690,7 @@ for thisPra_trial in pra_trials:
             textrest.setAutoDraw(True)
         if textrest.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > textrest.tStartRefresh + 1-frameTolerance:
+            if tThisFlipGlobal > textrest.tStartRefresh + 15-frameTolerance:
                 # keep track of stop time/frame for later
                 textrest.tStop = t  # not accounting for scr refresh
                 textrest.frameNStop = frameN  # exact frame index
@@ -699,7 +699,7 @@ for thisPra_trial in pra_trials:
                 textrest.setAutoDraw(False)
         
         # *textready* updates
-        if textready.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        if textready.status == NOT_STARTED and tThisFlip >= 15-frameTolerance:
             # keep track of start time/frame for later
             textready.frameNStart = frameN  # exact frame index
             textready.tStart = t  # local t and not account for scr refresh
@@ -710,7 +710,7 @@ for thisPra_trial in pra_trials:
             textready.setAutoDraw(True)
         if textready.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > textready.tStartRefresh + 1-frameTolerance:
+            if tThisFlipGlobal > textready.tStartRefresh + 5-frameTolerance:
                 # keep track of stop time/frame for later
                 textready.tStop = t  # not accounting for scr refresh
                 textready.frameNStop = frameN  # exact frame index
@@ -718,7 +718,7 @@ for thisPra_trial in pra_trials:
                 thisExp.timestampOnFlip(win, 'textready.stopped')
                 textready.setAutoDraw(False)
         # start/stop sound_2
-        if sound_2.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        if sound_2.status == NOT_STARTED and tThisFlip >= 15-frameTolerance:
             # keep track of start time/frame for later
             sound_2.frameNStart = frameN  # exact frame index
             sound_2.tStart = t  # local t and not account for scr refresh
@@ -763,7 +763,7 @@ for thisPra_trial in pra_trials:
     if routineForceEnded:
         routineTimer.reset()
     else:
-        routineTimer.addTime(-2.000000)
+        routineTimer.addTime(-20.000000)
     thisExp.nextEntry()
     
 # completed 1.0 repeats of 'pra_trials'
@@ -1208,7 +1208,7 @@ for thisTrial in trials:
     frameN = -1
     
     # --- Run Routine "rest" ---
-    while continueRoutine and routineTimer.getTime() < 2.0:
+    while continueRoutine and routineTimer.getTime() < 20.0:
         # get current time
         t = routineTimer.getTime()
         tThisFlip = win.getFutureFlipTime(clock=routineTimer)
@@ -1228,7 +1228,7 @@ for thisTrial in trials:
             textrest.setAutoDraw(True)
         if textrest.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > textrest.tStartRefresh + 1-frameTolerance:
+            if tThisFlipGlobal > textrest.tStartRefresh + 15-frameTolerance:
                 # keep track of stop time/frame for later
                 textrest.tStop = t  # not accounting for scr refresh
                 textrest.frameNStop = frameN  # exact frame index
@@ -1237,7 +1237,7 @@ for thisTrial in trials:
                 textrest.setAutoDraw(False)
         
         # *textready* updates
-        if textready.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        if textready.status == NOT_STARTED and tThisFlip >= 15-frameTolerance:
             # keep track of start time/frame for later
             textready.frameNStart = frameN  # exact frame index
             textready.tStart = t  # local t and not account for scr refresh
@@ -1248,7 +1248,7 @@ for thisTrial in trials:
             textready.setAutoDraw(True)
         if textready.status == STARTED:
             # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > textready.tStartRefresh + 1-frameTolerance:
+            if tThisFlipGlobal > textready.tStartRefresh + 5-frameTolerance:
                 # keep track of stop time/frame for later
                 textready.tStop = t  # not accounting for scr refresh
                 textready.frameNStop = frameN  # exact frame index
@@ -1256,7 +1256,7 @@ for thisTrial in trials:
                 thisExp.timestampOnFlip(win, 'textready.stopped')
                 textready.setAutoDraw(False)
         # start/stop sound_2
-        if sound_2.status == NOT_STARTED and tThisFlip >= 1-frameTolerance:
+        if sound_2.status == NOT_STARTED and tThisFlip >= 15-frameTolerance:
             # keep track of start time/frame for later
             sound_2.frameNStart = frameN  # exact frame index
             sound_2.tStart = t  # local t and not account for scr refresh
@@ -1301,7 +1301,7 @@ for thisTrial in trials:
     if routineForceEnded:
         routineTimer.reset()
     else:
-        routineTimer.addTime(-2.000000)
+        routineTimer.addTime(-20.000000)
     thisExp.nextEntry()
     
 # completed 1.0 repeats of 'trials'
