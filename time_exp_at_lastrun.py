ae87c57161ea7531fefb1ea207043097bc51c0c1
kao910716 <kao910716@gmail.com>
2023-05-31 21:17:43 +0800

    5/31

--- time_exp_at_lastrun.py
+++ time_exp_at_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2022.2.5),
-    on 五月 30, 2023, at 23:17
+    on 五月 31, 2023, at 09:52
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -55,7 +55,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\user\\Documents\\GitHub\\timeperception_at\\time_exp_at_lastrun.py',
+    originPath='G:\\timeperception\\timeperception_at\\time_exp_at_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
